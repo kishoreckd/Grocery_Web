20270bdd6f1011bf170aecde747e800188d46452
yuvarajcdckap <yuvarajcdckap@gmail.com>
2023-04-29 11:13:48 +0530

    home page whishlist added

--- home.js
+++ home.js
@@ -4,10 +4,9 @@ let vegetableDiv = document.querySelector('.vegitablesDiv')
 let fruitDiv = document.querySelector('.fruitsDiv')
 let viewmoreBtn = document.querySelector('.viewmoreBtn')
 let loader = document.querySelector('.fa-solid')
-let dDcategory=document.querySelector('.category')
-
-let dropDown=document.querySelector('.dropDown')
-
+let dDcategory = document.querySelector('.category')
+let toMoreAdd=document.querySelector('.toMore')
+let basketPrice=document.querySelector('.totalPr')
 
 window.addEventListener('DOMContentLoaded', () => {
     fetch(`http://localhost:3000/products`)
@@ -19,24 +18,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 // console.log(products[i].category);
                 if (products[i].category == 'vegetable' && i < 4) {
-                    vegitableOperation()
+                    vegitableOperation(vegetableDiv)
                     // console.log(products[i]);
 
                 }
+                else if (products[i].category == "fruits") {
 
-                // if (products[i].category == 'fruits' && i < 4) {
-                    // fruitsOperation()    
-                    // alert()
-                    // console.log(products[i]);
-                // }
+                    vegitableOperation(fruitDiv)
 
-                function vegitableOperation() {
+                }
+
+                function vegitableOperation(getDiv) {
                     let productId = products[i].id
                     let productsName = products[i].name
                     let productPrice = products[i].price
                     let productMrp = products[i].mrp
                     let productImg = products[i].images
-                    let discount=products[i].discount
+                    let discount = products[i].discount
 
 
                     //Element creation part
@@ -50,8 +48,8 @@ window.addEventListener('DOMContentLoaded', () => {
                     let offerImg = document.createElement('img')
                     offerImg.src = 'image/Star1.svg';
 
-                    let offPercent=document.createElement('span')
-                    offPercent.innerText=discount.slice(0,3)
+                    let offPercent = document.createElement('span')
+                    offPercent.innerText = discount.slice(0, 3)
 
                     let whisticonDiv = document.createElement('div')
                     whisticonDiv.setAttribute('class', 'heart')
@@ -77,21 +75,23 @@ window.addEventListener('DOMContentLoaded', () => {
                     let weightBtn = document.createElement('button')
                     let addBtn = document.createElement('button')
 
-                    let res = /,(.*\w+)/.exec(productsName)[1];
+                    let quantity = /,(.*\w+)/.exec(productsName)[1];
+                    let Pname = /(.*\w+),/.exec(productsName)[1];
+
 
                     // -----------------------------------------------------------------------//
                     // assigning part
-                    productName.innerText = productsName.slice(0,12)
+                    productName.innerText = Pname
                     rateTag.innerText = productPrice
                     discountTag.innerText = productMrp
-                    weightBtn.innerText = res
+                    weightBtn.innerText = quantity
                     addBtn.innerText = 'Add Item'
 
 
                     // -----------------------------------------------------------------------//
                     // Append the div in respenctive place part
 
-                    vegetableDiv.append(productInnerDiv)
+                    getDiv.append(productInnerDiv)
                     //   productsDiv.appendChild(productInnerDiv)
                     productInnerDiv.appendChild(iconDiv)
                     iconDiv.appendChild(offerImg)
@@ -110,35 +110,37 @@ window.addEventListener('DOMContentLoaded', () => {
                     productInnerDiv.appendChild(btnDiv)
                     btnDiv.appendChild(weightBtn)
                     btnDiv.appendChild(addBtn)
-
-                    whisticonDiv.addEventListener('click',(e)=>{
-                        if(hearticon.classList.contains('fa-regular')){
-                            hearticon.setAttribute('class','fa-solid fa-heart')
+                    
+                    whisticonDiv.addEventListener('click', (e) => {
+                        if (hearticon.classList.contains('fa-regular')) {
+                            hearticon.setAttribute('class', 'fa-solid fa-heart')
                         }
-                        else{
-                            hearticon.setAttribute('class','fa-regular fa-heart')
+                        else {
+                            hearticon.setAttribute('class', 'fa-regular fa-heart')
                         }
                     })
+
+                    addBtn.addEventListener('click',()=>{
+                      let weight=addBtn.previousSibling.innerText
+                    //   let ProPrice=
+                      const separatedWeight= weight.replace(/\D/g, '')
+                      
+                    })    
                 }
+
+
 
 
             }
-            dDcategory.addEventListener('click',()=>{
 
-                let categoryList=[]
-
-                for (let j = 0; j < products.length; j++) {
-                     categoryList.push(products[j].category)
-                }
-                for (let k = 0; k < categoryList.length; k++) {
-
-                }
-            })
-    
         })
 
 })
 
 
-// let input = "Kenya, Garden, PFO, Inv 2123, DG, Lot 5543, Ra";
+
+
+
+
+
 
